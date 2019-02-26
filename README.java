df256d8b279cc1475ee54ba8f8c924a4704cb2a8
weldro <43363914+weldro@users.noreply.github.com>
2019-02-27 01:38:00 +0530

    Update README.java

--- README.java
+++ README.java
@@ -1,5 +1,11 @@
 @SuppressLint("SimpleDateFormat")
 fun String.getDateAsText() : String {
     return java.text.SimpleDateFormat("MMMM dd,yyyy")
-        .format(java.text.SimpleDateFormat("MM/dd/yy").parse(this.replace("\\", ""))).toUpperCase().replace(",",", ")
+        .format(java.text.SimpleDateFormat("MM/dd/yy").parse(replace("\\", ""))).toUpperCase().replace(",",", ")
+}
+
+@SuppressLint("SimpleDateFormat")
+fun String.getFormattedAccountOpenDate() : String {
+    return java.text.SimpleDateFormat("MMMM dd,yy")
+        .format(java.text.SimpleDateFormat("MM/dd/yyyy").parse(replace("\\", "")))
 }
