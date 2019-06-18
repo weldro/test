09143a7954eee90b156a5709e401cbfe92e54b81
weldro <43363914+weldro@users.noreply.github.com>
2019-06-19 00:01:25 +0530

    Update README.java

--- README.java
+++ README.java
@@ -1,11 +1,12 @@
-@SuppressLint("SimpleDateFormat")
-fun String.getDateAsText() : String {
-    return java.text.SimpleDateFormat("MMMM dd,yyyy")
-        .format(java.text.SimpleDateFormat("MM/dd/yy").parse(replace("\\", ""))).toUpperCase().replace(",",", ")
-}
-
-@SuppressLint("SimpleDateFormat")
-fun String.getFormattedAccountOpenDate() : String {
-    return java.text.SimpleDateFormat("MMMM dd,yy")
-        .format(java.text.SimpleDateFormat("MM/dd/yyyy").parse(replace("\\", "")))
-}
+    fun getDateLabel(date:String,frequency: String) : String = if (!date.equals(getApplication<Application>().resources.getString(R.string.next_due_date))) {
+        when (frequency) {
+            EnvelopeFrequency.Weekly.toString() -> DateUtility.toWeekDay(date)
+            EnvelopeFrequency.Monthly.toString() -> DateUtility.toTheMonthDay(date)
+            EnvelopeFrequency.Monthly3.toString() -> DateUtility.toYearlyMonthDay(date)
+            EnvelopeFrequency.Monthly6.toString() -> DateUtility.toYearlyMonthDay(date)
+            EnvelopeFrequency.Yearly.toString() -> DateUtility.toYearlyMonthDay(date)
+            else -> ""
+        }
+    } else {
+        ""
+    }
